4e8965416b774be424d191dc6de7ff25eef38381
manal7h <manal.alsenani22@gmail.com>
2022-11-26 22:27:16 -0800

    include and exclude

--- src/JS/ApiJS.java
+++ src/JS/ApiJS.java
@@ -21,6 +21,7 @@ public class ApiJS implements Serializable{
 		this.info = info;
 	}
 	
+	
 
 	
 	
--- src/JS/RandomeUser.java
+++ src/JS/RandomeUser.java
@@ -25,8 +25,9 @@ public class RandomeUser implements Serializable {
 		HttpResponse<String> response = Client.send(request,HttpResponse.BodyHandlers.ofString()) ;
 		System.out.println(response.body());
 		ApiJS data=new Gson().fromJson(response.body(),ApiJS.class);
+		
 		for (Integer i = 0; i < response.body().length(); i++){
-			System.out.println("This is Json Data:");
+			System.out.println("Name"+data.getResults().get(0).getName());
 			System.out.println("Info-Results:" +" "+ data.getInfo().getResults());
 			System.out.println("Info-Version:" +" "+ data.getInfo().getVersion());
 			System.out.println("Results-Gender:" +" "+ data.getResults().get(i).getGender());
@@ -45,27 +46,62 @@ public class RandomeUser implements Serializable {
     	Integer page = sc.nextInt();
     	System.out.println("Enter nationality:");
     	String nat = sc.next();
-    	
+    	System.out.println("Enter 1 if you want to include or 2 to exclude:");
+    	Integer choise = sc.nextInt();
+    	if(choise==1) {
+    	  	System.out.println("Enter Types you want to include :");
+        	Integer inc = sc.nextInt();	
 	    for (Integer i = 0; i < results; i++){
 	    	
 	    	HttpClient Client=HttpClient.newHttpClient();
 	    	HttpRequest request = HttpRequest.newBuilder()
-					.uri(URI.create("https://randomuser.me/api/?results="+results+"&password="+password+"&seed"+seed+"&psge"+page+"&nat"+nat))
+					.uri(URI.create("https://randomuser.me/api/?results="+results+"&password="+password+"&seed="+seed+"&psge="+page+"&nat="+nat+"&inc="+inc))
 //					.method("GET", HttpRequest.BodyPublishers.noBody())
 					.build();
+	    	
+	    	HttpResponse<String> response =Client.send(request,HttpResponse.BodyHandlers.ofString());
+	    	ApiJS data=new Gson().fromJson(response.body(),ApiJS.class);
+	    	
+	
+	System.out.println("This is Json Data:");
+    
+	System.out.println("Info-Results:" +" "+ data.getInfo().getResults());
+	
+    System.out.println("Info-Version:" +" "+ data.getInfo().getVersion());
+    
+    if(data.getResults().get(i).getGender()!=null) {
+    System.out.println("Results-Gender:" +" "+ data.getResults().get(i).getGender());}
+    
+    if(data.getResults().get(i).getLogin().getPassword()!=null) {
+    System.out.println("Results-Password:" +" "+ data.getResults().get(i).getLogin().getPassword());}
+    
+	System.out.println("Info-Seed:" +" "+ data.getInfo().getSeed());
+	System.out.println("Info-Page:" +" "+ data.getInfo().getPage());
+	
+	}}
+    	else {
+	    for (Integer i = 0; i < results; i++){
+	 	  	System.out.println("Enter Types you want to exclude :");
+        	Integer exc = sc.nextInt();	
+	    	HttpClient Client=HttpClient.newHttpClient();
+	    	HttpRequest request = HttpRequest.newBuilder()
+					.uri(URI.create("https://randomuser.me/api/?results="+results+"&password="+password+"&seed="+seed+"&psge="+page+"&nat="+nat+"&exc="+exc))
+//					.method("GET", HttpRequest.BodyPublishers.noBody())
+					.build();
+	    	
 	    	HttpResponse<String> response =Client.send(request,HttpResponse.BodyHandlers.ofString());
 	    	ApiJS data=new Gson().fromJson(response.body(),ApiJS.class);
 	    	
 	    	
 	System.out.println("This is Json Data:");
 	System.out.println("Info-Results:" +" "+ data.getInfo().getResults());
-	System.out.println("Info-Version:" +" "+ data.getInfo().getVersion());
-	System.out.println("Results-Gender:" +" "+ data.getResults().get(i).getGender());
-	System.out.println("Results-Password:" +" "+ data.getResults().get(i).getLogin().getPassword());
+    System.out.println("Info-Version:" +" "+ data.getInfo().getVersion());
+    System.out.println("Results-Gender:" +" "+ data.getResults().get(i).getGender());
+    System.out.println("Results-Password:" +" "+ data.getResults().get(i).getLogin().getPassword());
 	System.out.println("Info-Seed:" +" "+ data.getInfo().getSeed());
 	System.out.println("Info-Page:" +" "+ data.getInfo().getPage());
+	    }
 	}
-	}
-
+		}
 }
 }
