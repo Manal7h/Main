ee87bdfae5f9ff7a71fefff5b160837ba9273a9b
manal7h <manal.alsenani22@gmail.com>
2022-11-27 02:29:37 -0800

    faiza

--- src/JS/RandomeUser.java
+++ src/JS/RandomeUser.java
@@ -17,91 +17,199 @@ public class RandomeUser implements Serializable {
 		
 
 	if(results==0){
-		 HttpClient Client=HttpClient.newHttpClient();
-		 HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create("https://randomuser.me/api/?results=50"))
-//				.("GET", HttpRequest.BodyPublishers.noBody())
-				.build();
-		HttpResponse<String> response = Client.send(request,HttpResponse.BodyHandlers.ofString()) ;
-		System.out.println(response.body());
-		ApiJS data=new Gson().fromJson(response.body(),ApiJS.class);
-		
-		for (Integer i = 0; i < response.body().length(); i++){
-			System.out.println("Name"+data.getResults().get(0).getName());
-			System.out.println("Info-Results:" +" "+ data.getInfo().getResults());
-			System.out.println("Info-Version:" +" "+ data.getInfo().getVersion());
-			System.out.println("Results-Gender:" +" "+ data.getResults().get(i).getGender());
-			System.out.println("Results-Password:" +" "+ data.getResults().get(i).getLogin().getPassword());
-			System.out.println("Info-Seed:" +" "+ data.getInfo().getSeed());
-			System.out.println("Info-Page:" +" "+ data.getInfo().getPage());
-		}
+		 HttpClient client = HttpClient.newHttpClient();
+		    HttpRequest request = HttpRequest.newBuilder()
+		    .uri(URI.create("https://randomuser.me/api/?results=50"))
+		    .build();
+		    HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+		    ApiJS data=new Gson().fromJson(response.body(),ApiJS.class);
+		    
+		    
+		    
+		    for(Integer i=0; i<= response.body().length();i++) {
+			System.out.println("Email:        "+data.getResults().get(i).getEmail());
+			System.out.println("gender:       "+data.getResults().get(i).getGender());
+			System.out.println("nat:          "+data.getResults().get(i).getNat());
+			System.out.println("password:     "+data.getResults().get(i).getLogin().getPassword());
+			System.out.println("seed:         "+data.getInfo().getSeed());
+			System.out.println("results:      "+data.getInfo().getResults());
+			System.out.println("page:         "+data.getInfo().getPage());
+			System.out.println("**********************************************************\n \n \n");
+		    }
 	}
-	
-	else {
-		System.out.println("Which Password you want to appear:");
-    	String password = sc.next();
-    	System.out.println("Enter Seed:");
-    	String seed = sc.next();
-    	System.out.println("Enter Page num:");
-    	Integer page = sc.nextInt();
-    	System.out.println("Enter nationality:");
-    	String nat = sc.next();
-    	System.out.println("Enter 1 if you want to include or 2 to exclude:");
-    	Integer choise = sc.nextInt();
-    	if(choise==1) {
-    	  	System.out.println("Enter Types you want to include :");
-        	Integer inc = sc.nextInt();	
-	    for (Integer i = 0; i < results; i++){
-	    	
-	    	HttpClient Client=HttpClient.newHttpClient();
-	    	HttpRequest request = HttpRequest.newBuilder()
-					.uri(URI.create("https://randomuser.me/api/?results="+results+"&password="+password+"&seed="+seed+"&psge="+page+"&nat="+nat+"&inc="+inc))
-//					.method("GET", HttpRequest.BodyPublishers.noBody())
-					.build();
-	    	
-	    	HttpResponse<String> response =Client.send(request,HttpResponse.BodyHandlers.ofString());
-	    	ApiJS data=new Gson().fromJson(response.body(),ApiJS.class);
-	    	
-	
-	System.out.println("This is Json Data:");
-    
-	System.out.println("Info-Results:" +" "+ data.getInfo().getResults());
-	
-    System.out.println("Info-Version:" +" "+ data.getInfo().getVersion());
-    
-    if(data.getResults().get(i).getGender()!=null) {
-    System.out.println("Results-Gender:" +" "+ data.getResults().get(i).getGender());}
-    
-    if(data.getResults().get(i).getLogin().getPassword()!=null) {
-    System.out.println("Results-Password:" +" "+ data.getResults().get(i).getLogin().getPassword());}
-    
-	System.out.println("Info-Seed:" +" "+ data.getInfo().getSeed());
-	System.out.println("Info-Page:" +" "+ data.getInfo().getPage());
-	
-	}}
     	else {
-	    for (Integer i = 0; i < results; i++){
-	 	  	System.out.println("Enter Types you want to exclude :");
-        	Integer exc = sc.nextInt();	
-	    	HttpClient Client=HttpClient.newHttpClient();
-	    	HttpRequest request = HttpRequest.newBuilder()
-					.uri(URI.create("https://randomuser.me/api/?results="+results+"&password="+password+"&seed="+seed+"&psge="+page+"&nat="+nat+"&exc="+exc))
-//					.method("GET", HttpRequest.BodyPublishers.noBody())
-					.build();
-	    	
-	    	HttpResponse<String> response =Client.send(request,HttpResponse.BodyHandlers.ofString());
-	    	ApiJS data=new Gson().fromJson(response.body(),ApiJS.class);
-	    	
-	    	
-	System.out.println("This is Json Data:");
-	System.out.println("Info-Results:" +" "+ data.getInfo().getResults());
-    System.out.println("Info-Version:" +" "+ data.getInfo().getVersion());
-    System.out.println("Results-Gender:" +" "+ data.getResults().get(i).getGender());
-    System.out.println("Results-Password:" +" "+ data.getResults().get(i).getLogin().getPassword());
-	System.out.println("Info-Seed:" +" "+ data.getInfo().getSeed());
-	System.out.println("Info-Page:" +" "+ data.getInfo().getPage());
-	    }
+    		for(Integer i=0; i<results ;i++) {
+    			 HttpClient client = HttpClient.newHttpClient();
+    			    HttpRequest request = HttpRequest.newBuilder()
+    			    .uri(URI.create("https://randomuser.me/api/?results=50"))
+    			    .build();
+    			    HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+    			    ApiJS data=new Gson().fromJson(response.body(),ApiJS.class);
+    			    
+    			    
+    			    
+    			   
+    				System.out.println("Email:        "+data.getResults().get(i).getEmail());
+    				System.out.println("gender:       "+data.getResults().get(i).getGender());
+    				System.out.println("nat:          "+data.getResults().get(i).getNat());
+    				System.out.println("password:     "+data.getResults().get(i).getLogin().getPassword());
+    				System.out.println("seed:         "+data.getInfo().getSeed());
+    				System.out.println("results:      "+data.getInfo().getResults());
+    				System.out.println("page:         "+data.getInfo().getPage());
+    				System.out.println("**********************************************************\n \n \n");
+    			    }
+    		
+    	}
+    		
+    		
+    		
+//    		Done by faiza=========================================
+    		System.out.println("Which Password you want to appear:");
+        	String password = sc.next();
+        	HttpClient client = HttpClient.newHttpClient();
+    		HttpRequest request = HttpRequest.newBuilder()
+    		.uri(URI.create("https://randomuser.me/api/?password="+password))
+    		.build();
+    		HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+    		ApiJS data=new Gson().fromJson(response.body(),ApiJS.class);
+    		System.out.println("password:     "+data.getResults().get(0).getLogin().getPassword());
+        	
+        	
+        	
+        	
+        	System.out.println("Enter Seed:");
+        	String seed = sc.next();
+        	HttpClient client1 = HttpClient.newHttpClient();
+    		HttpRequest request1 = HttpRequest.newBuilder()
+    		.uri(URI.create("https://randomuser.me/api/?seed="+seed))
+    		.build();
+    		HttpResponse<String> response1 = client1.send(request1,HttpResponse.BodyHandlers.ofString());
+    		ApiJS data1=new Gson().fromJson(response.body(),ApiJS.class);
+    		System.out.println("seed:         "+data1.getInfo().getSeed());
+        	
+        	
+        	
+    
+        	System.out.println("Enter Page num:");
+        	Integer page = sc.nextInt();
+        	HttpClient client2 = HttpClient.newHttpClient();
+    		HttpRequest request2 = HttpRequest.newBuilder()
+    		.uri(URI.create("https://randomuser.me/api/?page="+page))
+    		.build();
+    		HttpResponse<String> response2 = client2.send(request2,HttpResponse.BodyHandlers.ofString());
+    		ApiJS data2=new Gson().fromJson(response2.body(),ApiJS.class);
+    		System.out.println("page:         "+data2.getInfo().getPage());
+        	
+        	
+        	System.out.println("Enter nationality:");
+        	String nat = sc.next();
+        	HttpClient client0 = HttpClient.newHttpClient();
+    		HttpRequest request0 = HttpRequest.newBuilder()
+    		.uri(URI.create("https://randomuser.me/api/?nat="+nat))
+    		.build();
+    		HttpResponse<String> response0 = client0.send(request0,HttpResponse.BodyHandlers.ofString());
+    		ApiJS data0=new Gson().fromJson(response0.body(),ApiJS.class);
+    		System.out.println("nat:          "+data0.getResults().get(0).getNat());
+        	
+        	
+        	
+        	System.out.println("Enter gender:");
+        	String gender = sc.next();
+        	HttpClient client4 = HttpClient.newHttpClient();
+    		HttpRequest request4 = HttpRequest.newBuilder()
+    		.uri(URI.create("https://randomuser.me/api/?gender="+gender))
+    		.build();
+    		HttpResponse<String> response4 = client4.send(request4,HttpResponse.BodyHandlers.ofString());
+    		ApiJS data4=new Gson().fromJson(response4.body(),ApiJS.class);
+    		System.out.println("gender:       "+data4.getResults().get(0).getGender());
+    		
+
+        	System.out.println("what you want to include?");
+        	System.out.println("email");
+        	System.out.println("cell");
+        	System.out.println("gender");
+        	System.out.println("nat");
+        	System.out.println("passward");
+        	String include = sc.next();
+        	HttpClient client3 = HttpClient.newHttpClient();
+    		HttpRequest request3 = HttpRequest.newBuilder()
+    		.uri(URI.create("https://randomuser.me/api/?inc="+include))
+    		.build();
+    		HttpResponse<String> response3 = client3.send(request3,HttpResponse.BodyHandlers.ofString());
+    		ApiJS data3=new Gson().fromJson(response3.body(),ApiJS.class);
+    		if(data3.getResults().get(0).getEmail()!=null) {
+    			System.out.println("Email:        "+data3.getResults().get(0).getEmail());}
+    				if(data3.getResults().get(0).getCell()!=null) {
+    			System.out.println("cell:         "+data3.getResults().get(0).getCell());}
+    			if(data3.getResults().get(0).getGender()!=null) {
+    			System.out.println("gender:       "+data3.getResults().get(0).getGender());}
+    			if(data3.getResults().get(0).getNat()!=null) {
+    			System.out.println("nat:          "+data3.getResults().get(0).getNat());}
+    			if(data3.getResults().get(0).getLogin().getPassword()!=null) {
+    				System.out.println("password:     "+data3.getResults().get(0).getLogin().getPassword());}
+ 		    
+ 		    
+ 		    
+ 		    
+ 		    
+ 		    
+ 		    
+ 		    
+ 		    
+ 		    
+ 		    
+//        	if( choice==1) {
+//		    	 System.out.println("enter type you want to exc");
+//		    	 String exc = sc.next();
+//		HttpClient client = HttpClient.newHttpClient();
+//		HttpRequest request = HttpRequest.newBuilder()
+//		.uri(URI.create("https://randomuser.me/api/?results="+results+"&gender="+gender+"&password="+password+"&seed="+seed+ "&nat="+nat+ "&page="+page+"&exc="+exc))
+//		.build();
+//		HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+//		ApiJS data=new Gson().fromJson(response.body(),ApiJS.class);
+//		
+//		     
+//
+//		
+//		for(Integer i=0; i<results ;i++) {
+//			if(data.getResults().get(i).getEmail()!=null) {
+//		System.out.println("Email:        "+data.getResults().get(i).getEmail());}
+//			if(data.getResults().get(i).getCell()!=null) {
+//		System.out.println("cell:         "+data.getResults().get(i).getCell());}
+//		if(data.getResults().get(i).getGender()!=null) {
+//		System.out.println("gender:       "+data.getResults().get(i).getGender());}
+//		if(data.getResults().get(i).getNat()!=null) {
+//		System.out.println("nat:          "+data.getResults().get(i).getNat());}
+//		if(data.getResults().get(i).getLogin().getPassword()!=null) {
+//			System.out.println("password:     "+data.getResults().get(i).getLogin().getPassword());}
+//		}
+//        	}else {
+//			System.out.println("enter type you want to inc");
+//			String inc = sc.next();
+//			HttpClient Client=HttpClient.newHttpClient();
+//	    	HttpRequest request = HttpRequest.newBuilder()
+//					.uri(URI.create("https://randomuser.me/api/?results="+results+"&password="+password+"&seed="+seed+"&psge="+page+"&nat="+nat+"&inc="+inc))
+////					.method("GET", HttpRequest.BodyPublishers.noBody())
+//					.build();
+//
+//	    	HttpResponse<String> response =Client.send(request,HttpResponse.BodyHandlers.ofString());
+//	    	ApiJS data=new Gson().fromJson(response.body(),ApiJS.class);
+//			
+//			for(Integer i=0; i<results ;i++) {
+//				if(data.getResults().get(i).getEmail()!=null) {
+//			System.out.println("Email:        "+data.getResults().get(i).getEmail());}
+//				if(data.getResults().get(i).getCell()!=null) {
+//			System.out.println("cell:         "+data.getResults().get(i).getCell());}
+//			if(data.getResults().get(i).getGender()!=null) {
+//			System.out.println("gender:       "+data.getResults().get(i).getGender());}
+//			if(data.getResults().get(i).getNat()!=null) {
+//			System.out.println("nat:          "+data.getResults().get(i).getNat());}
+//			if(data.getResults().get(i).getLogin().getPassword()!=null) {
+//				System.out.println("password:     "+data.getResults().get(i).getLogin().getPassword());}
+//			
+//		}
+//	
+//	    }
 	}
-		}
-}
-}
+	}
+
